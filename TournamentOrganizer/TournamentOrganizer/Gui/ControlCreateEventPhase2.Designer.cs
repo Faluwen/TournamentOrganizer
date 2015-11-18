027341a22411e02dfe442bcee2df49dff8d76790
Christopher Schütz <faluwen@gmail.com>
2015-11-18 11:03:36 +0100

    fehlende Designer

---
+++ TournamentOrganizer/TournamentOrganizer/Gui/ControlCreateEventPhase2.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace EventOrganizer.Gui
+{
+    partial class ControlCreateEventPhase2
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelUeberschrift = new System.Windows.Forms.Panel();
+            this.labelUeberschrift = new System.Windows.Forms.Label();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonWeiter = new System.Windows.Forms.Button();
+            this.controlCreateEventPhase2RoundRobin1 = new EventOrganizer.Gui.ControlCreateEventPhase2RoundRobin();
+            this.panelUeberschrift.SuspendLayout();
+            this.panelContent.SuspendLayout();
+            this.panelButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelUeberschrift
+            // 
+            this.panelUeberschrift.Controls.Add(this.labelUeberschrift);
+            this.panelUeberschrift.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelUeberschrift.Location = new System.Drawing.Point(0, 0);
+            this.panelUeberschrift.Name = "panelUeberschrift";
+            this.panelUeberschrift.Size = new System.Drawing.Size(800, 100);
+            this.panelUeberschrift.TabIndex = 0;
+            // 
+            // labelUeberschrift
+            // 
+            this.labelUeberschrift.AutoSize = true;
+            this.labelUeberschrift.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUeberschrift.Location = new System.Drawing.Point(117, 32);
+            this.labelUeberschrift.Name = "labelUeberschrift";
+            this.labelUeberschrift.Size = new System.Drawing.Size(547, 39);
+            this.labelUeberschrift.TabIndex = 1;
+            this.labelUeberschrift.Text = "Eventerstellung Phase 2/3, Stage X/X";
+            // 
+            // panelContent
+            // 
+            this.panelContent.Controls.Add(this.controlCreateEventPhase2RoundRobin1);
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(0, 100);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(800, 275);
+            this.panelContent.TabIndex = 1;
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.Controls.Add(this.buttonBack);
+            this.panelButtons.Controls.Add(this.buttonWeiter);
+            this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelButtons.Location = new System.Drawing.Point(0, 375);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(800, 75);
+            this.panelButtons.TabIndex = 1;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonBack.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBack.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBack.Location = new System.Drawing.Point(18, 11);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(140, 50);
+            this.buttonBack.TabIndex = 21;
+            this.buttonBack.Text = "Zurück";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            // 
+            // buttonWeiter
+            // 
+            this.buttonWeiter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonWeiter.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.buttonWeiter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonWeiter.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonWeiter.Location = new System.Drawing.Point(645, 11);
+            this.buttonWeiter.Name = "buttonWeiter";
+            this.buttonWeiter.Size = new System.Drawing.Size(140, 50);
+            this.buttonWeiter.TabIndex = 20;
+            this.buttonWeiter.Text = "Weiter";
+            this.buttonWeiter.UseVisualStyleBackColor = false;
+            this.buttonWeiter.Click += new System.EventHandler(this.buttonWeiter_Click);
+            // 
+            // controlCreateEventPhase2RoundRobin1
+            // 
+            this.controlCreateEventPhase2RoundRobin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlCreateEventPhase2RoundRobin1.Location = new System.Drawing.Point(0, 0);
+            this.controlCreateEventPhase2RoundRobin1.Name = "controlCreateEventPhase2RoundRobin1";
+            this.controlCreateEventPhase2RoundRobin1.Size = new System.Drawing.Size(800, 275);
+            this.controlCreateEventPhase2RoundRobin1.TabIndex = 0;
+            // 
+            // ControlCreateEventPhase2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.panelUeberschrift);
+            this.Name = "ControlCreateEventPhase2";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.panelUeberschrift.ResumeLayout(false);
+            this.panelUeberschrift.PerformLayout();
+            this.panelContent.ResumeLayout(false);
+            this.panelButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelUeberschrift;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonWeiter;
+        private System.Windows.Forms.Label labelUeberschrift;
+        private ControlCreateEventPhase2RoundRobin controlCreateEventPhase2RoundRobin1;
+    }
+}
---
+++ TournamentOrganizer/TournamentOrganizer/Gui/ControlCreateEventPhase2RoundRobin.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace EventOrganizer.Gui
+{
+    partial class ControlCreateEventPhase2RoundRobin
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainerContent = new System.Windows.Forms.SplitContainer();
+            this.comboBoxPlayers = new System.Windows.Forms.ComboBox();
+            this.labelBoX = new System.Windows.Forms.Label();
+            this.comboBoxMatchesBoX = new System.Windows.Forms.ComboBox();
+            this.comboBoxGroups = new System.Windows.Forms.ComboBox();
+            this.labelFormat = new System.Windows.Forms.Label();
+            this.comboBoxFormat = new System.Windows.Forms.ComboBox();
+            this.labelPlayers = new System.Windows.Forms.Label();
+            this.labelGroups = new System.Windows.Forms.Label();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.textBoxTimeRound = new System.Windows.Forms.TextBox();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelTimeRound = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerContent)).BeginInit();
+            this.splitContainerContent.Panel1.SuspendLayout();
+            this.splitContainerContent.Panel2.SuspendLayout();
+            this.splitContainerContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainerContent
+            // 
+            this.splitContainerContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerContent.IsSplitterFixed = true;
+            this.splitContainerContent.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerContent.Name = "splitContainerContent";
+            // 
+            // splitContainerContent.Panel1
+            // 
+            this.splitContainerContent.Panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainerContent.Panel1.Controls.Add(this.comboBoxPlayers);
+            this.splitContainerContent.Panel1.Controls.Add(this.labelBoX);
+            this.splitContainerContent.Panel1.Controls.Add(this.comboBoxMatchesBoX);
+            this.splitContainerContent.Panel1.Controls.Add(this.comboBoxGroups);
+            this.splitContainerContent.Panel1.Controls.Add(this.labelFormat);
+            this.splitContainerContent.Panel1.Controls.Add(this.comboBoxFormat);
+            this.splitContainerContent.Panel1.Controls.Add(this.labelPlayers);
+            this.splitContainerContent.Panel1.Controls.Add(this.labelGroups);
+            // 
+            // splitContainerContent.Panel2
+            // 
+            this.splitContainerContent.Panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainerContent.Panel2.Controls.Add(this.textBoxInfo);
+            this.splitContainerContent.Panel2.Controls.Add(this.labelInfo);
+            this.splitContainerContent.Panel2.Controls.Add(this.textBoxTimeRound);
+            this.splitContainerContent.Panel2.Controls.Add(this.textBoxTime);
+            this.splitContainerContent.Panel2.Controls.Add(this.dateTimePickerDate);
+            this.splitContainerContent.Panel2.Controls.Add(this.labelTime);
+            this.splitContainerContent.Panel2.Controls.Add(this.labelTimeRound);
+            this.splitContainerContent.Panel2.Controls.Add(this.labelDate);
+            this.splitContainerContent.Size = new System.Drawing.Size(800, 275);
+            this.splitContainerContent.SplitterDistance = 398;
+            this.splitContainerContent.TabIndex = 5;
+            // 
+            // comboBoxPlayers
+            // 
+            this.comboBoxPlayers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxPlayers.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxPlayers.FormattingEnabled = true;
+            this.comboBoxPlayers.Location = new System.Drawing.Point(18, 214);
+            this.comboBoxPlayers.Name = "comboBoxPlayers";
+            this.comboBoxPlayers.Size = new System.Drawing.Size(365, 24);
+            this.comboBoxPlayers.TabIndex = 14;
+            // 
+            // labelBoX
+            // 
+            this.labelBoX.AutoSize = true;
+            this.labelBoX.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBoX.Location = new System.Drawing.Point(15, 135);
+            this.labelBoX.Name = "labelBoX";
+            this.labelBoX.Size = new System.Drawing.Size(64, 16);
+            this.labelBoX.TabIndex = 13;
+            this.labelBoX.Text = "Best of X:";
+            // 
+            // comboBoxMatchesBoX
+            // 
+            this.comboBoxMatchesBoX.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxMatchesBoX.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxMatchesBoX.FormattingEnabled = true;
+            this.comboBoxMatchesBoX.Location = new System.Drawing.Point(18, 154);
+            this.comboBoxMatchesBoX.Name = "comboBoxMatchesBoX";
+            this.comboBoxMatchesBoX.Size = new System.Drawing.Size(365, 24);
+            this.comboBoxMatchesBoX.TabIndex = 12;
+            // 
+            // comboBoxGroups
+            // 
+            this.comboBoxGroups.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxGroups.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxGroups.FormattingEnabled = true;
+            this.comboBoxGroups.Location = new System.Drawing.Point(18, 34);
+            this.comboBoxGroups.Name = "comboBoxGroups";
+            this.comboBoxGroups.Size = new System.Drawing.Size(365, 24);
+            this.comboBoxGroups.TabIndex = 3;
+            // 
+            // labelFormat
+            // 
+            this.labelFormat.AutoSize = true;
+            this.labelFormat.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFormat.Location = new System.Drawing.Point(15, 75);
+            this.labelFormat.Name = "labelFormat";
+            this.labelFormat.Size = new System.Drawing.Size(54, 16);
+            this.labelFormat.TabIndex = 2;
+            this.labelFormat.Text = "Format:";
+            // 
+            // comboBoxFormat
+            // 
+            this.comboBoxFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxFormat.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFormat.FormattingEnabled = true;
+            this.comboBoxFormat.Location = new System.Drawing.Point(18, 94);
+            this.comboBoxFormat.Name = "comboBoxFormat";
+            this.comboBoxFormat.Size = new System.Drawing.Size(365, 24);
+            this.comboBoxFormat.TabIndex = 11;
+            // 
+            // labelPlayers
+            // 
+            this.labelPlayers.AutoSize = true;
+            this.labelPlayers.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlayers.Location = new System.Drawing.Point(15, 195);
+            this.labelPlayers.Name = "labelPlayers";
+            this.labelPlayers.Size = new System.Drawing.Size(270, 16);
+            this.labelPlayers.TabIndex = 5;
+            this.labelPlayers.Text = "Wie viele Spieler kommen pro Gruppe weiter:";
+            // 
+            // labelGroups
+            // 
+            this.labelGroups.AutoSize = true;
+            this.labelGroups.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGroups.Location = new System.Drawing.Point(15, 15);
+            this.labelGroups.Name = "labelGroups";
+            this.labelGroups.Size = new System.Drawing.Size(103, 16);
+            this.labelGroups.TabIndex = 1;
+            this.labelGroups.Text = "Anzahl Gruppen:";
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInfo.Enabled = false;
+            this.textBoxInfo.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInfo.Location = new System.Drawing.Point(18, 214);
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(365, 23);
+            this.textBoxInfo.TabIndex = 17;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo.Location = new System.Drawing.Point(15, 195);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(30, 16);
+            this.labelInfo.TabIndex = 16;
+            this.labelInfo.Text = "Info";
+            // 
+            // textBoxTimeRound
+            // 
+            this.textBoxTimeRound.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTimeRound.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTimeRound.Location = new System.Drawing.Point(18, 154);
+            this.textBoxTimeRound.Name = "textBoxTimeRound";
+            this.textBoxTimeRound.Size = new System.Drawing.Size(365, 23);
+            this.textBoxTimeRound.TabIndex = 15;
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTime.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTime.Location = new System.Drawing.Point(18, 94);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(365, 23);
+            this.textBoxTime.TabIndex = 7;
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePickerDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerDate.Location = new System.Drawing.Point(18, 34);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(365, 22);
+            this.dateTimePickerDate.TabIndex = 4;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(15, 75);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(61, 16);
+            this.labelTime.TabIndex = 6;
+            this.labelTime.Text = "Startzeit:";
+            // 
+            // labelTimeRound
+            // 
+            this.labelTimeRound.AutoSize = true;
+            this.labelTimeRound.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimeRound.Location = new System.Drawing.Point(15, 135);
+            this.labelTimeRound.Name = "labelTimeRound";
+            this.labelTimeRound.Size = new System.Drawing.Size(149, 16);
+            this.labelTimeRound.TabIndex = 14;
+            this.labelTimeRound.Text = "Rundenzeit: (in Minuten)";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(15, 15);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(77, 16);
+            this.labelDate.TabIndex = 13;
+            this.labelDate.Text = "Startdatum:";
+            // 
+            // ControlCreateEventPhase2RoundRobin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainerContent);
+            this.Name = "ControlCreateEventPhase2RoundRobin";
+            this.Size = new System.Drawing.Size(800, 275);
+            this.splitContainerContent.Panel1.ResumeLayout(false);
+            this.splitContainerContent.Panel1.PerformLayout();
+            this.splitContainerContent.Panel2.ResumeLayout(false);
+            this.splitContainerContent.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerContent)).EndInit();
+            this.splitContainerContent.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainerContent;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelPlayers;
+        private System.Windows.Forms.ComboBox comboBoxGroups;
+        private System.Windows.Forms.Label labelFormat;
+        private System.Windows.Forms.ComboBox comboBoxFormat;
+        private System.Windows.Forms.Label labelGroups;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+        private System.Windows.Forms.Label labelTimeRound;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelBoX;
+        private System.Windows.Forms.ComboBox comboBoxMatchesBoX;
+        private System.Windows.Forms.ComboBox comboBoxPlayers;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.TextBox textBoxTimeRound;
+    }
+}
